764ef745d0a7cb80a14a36db9af8f3d5359240e9
olivia <soylee0314@gmail.com>
2020-06-01 18:01:09 +0900

    - kotlin pair collection 사용하여 코드 변경

--- app/src/main/java/net/soy/algorithm/level2/stack_queue/printer/Solution.kt
+++ app/src/main/java/net/soy/algorithm/level2/stack_queue/printer/Solution.kt
@@ -11,76 +11,33 @@ import java.util.*
 class Solution {
 
     companion object{
-        lateinit var queue : Queue<Printer>
-
-        /**
-         * @param num 큐 가장 앞에 있던 데이터의 우선순위
-         * @return
-         * num 보다 우선순위가 높은 데이터 존재할 경우 true
-         * 존재하지 않을 경우 false
-         */
-        fun check(num: Int): Boolean{
-            var flag = false
-
-
-            val checkList = queue.toTypedArray()
-
-            for(i in queue.indices){
-                if(checkList[i].priority > num){
-                    flag = true
-                    break
-                }
-            }
-            return flag
-        }
+        lateinit var queue : Queue<Pair<Int, Int>>
     }
 
     fun solution(priorities: IntArray, location: Int): Int {
-        var answerList = arrayOfNulls<Int>(priorities.size)
         var answer = 0
         queue = LinkedList()
 
         // queue 에 삽입
         for(i in priorities.indices){
-            queue.offer(Printer(priorities[i], i))
+            queue.offer(Pair(priorities[i], i))
         }
 
-        var j = 0
         while (!queue.isEmpty()){
             //1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
             val printer = queue.poll()
-            val priority: Int = printer.priority
 
             //2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
-            if(check(priority)){
+            if(queue.any { printer.first < it.first }){
                 queue.offer(printer)
-            //3. 그렇지 않으면 J를 인쇄합니다. (내가 인쇄 요청한 문서가 몇번째인지 알기 위해서 정답 리스트에 넣은뒤 다시 비교 작업을 해야함)
+            //3. 그렇지 않으면 J 를 인쇄합니다.
             } else {
-                answerList[j] = printer.index
-                j++
-            }
-        }
-
-
-        for(i in answerList.indices){
-            //내가 인쇄 요청한 문서의 위치 (location) 와 같을 경우 answer 값 리턴
-            // 응답 값을 location 값에서 + 1 해준다
-            if(answerList[i] == location){
-                answer = i + 1
-                break
+                answer++
+                // 내가 인쇄 요청한 문서의 위치와 현재 인덱스가 같을경우 answer 리턴함
+                if(printer.second == location) return answer
             }
         }
 
         return answer
     }
-
-    /**
-     * 우선순위와 인덱스 값으로 구성된 Printer 클래스
-     */
-    data class Printer(
-        val priority: Int,
-        val index: Int
-    )
-
-
 }

6af2a77d44e31d505c789f5787836d6b5138b830
olivia <soylee0314@gmail.com>
2020-06-01 19:10:45 +0900

    - 코딩테스트 문제 연습 "멀쩡한 사각형 "

---
+++ app/src/main/java/net/soy/algorithm/level2/summer_winter_coding_2019/a_fine_square/Solution.kt
@@ -0,0 +1,38 @@
+package net.soy.algorithm.level2.summer_winter_coding_2019.a_fine_square
+
+import java.util.*
+
+/**
+ * Class: printer
+ * Created by leesoyoung on 2020/06/01.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 level2 Summer/Winter Coding(2019) "멀쩡한 사각형"
+ *
+ * 1. 사각형의 최소단위를 생각한다 > 가로, 세로를 각각 최대 공약수로 나눈값
+ *
+ * 2. 예를들어 최소단위가 w : 2, h : 3 이 된다면 점선은 가로 2개 세로 3개의 사각형을 지나가고 그중 가로 세로가 겹치는 부분이 하나가 있으므로 - 1 을 해준다
+ *    즉, (2+ 3 - 1)
+ *
+ * 3. 사각형을 최소단위로 생각하였으므로 그 값에서 다시 최대 공약수를 곱하면 빠지는 사각형의 갯수가 된다
+ *
+ *
+ * 4. 즉 빠지는 사각형의 갯수는 (w/gcd + h/gcd - 1)* gcd = w + h - gcd
+ *
+ *
+ */
+class Solution {
+
+
+    fun solution(w: Int, h: Int): Long {
+        return w*h - (w + h - gcd(w.toLong(), h.toLong()))
+    }
+
+    /**
+     *  최대 공약수 구하는 메소드
+     *  큰수를 작은 값으로 나눈 값이 0일 경우 작은값이 최대 공약수
+     *  아닐경우 변경하여 과정을 반복하면 된다
+     */
+    fun gcd(a: Long, b: Long): Long{
+        return if( b == 0L) a else gcd(b, a%b)
+    }
+}

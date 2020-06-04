05ab060944bf20891ec0359c79594d84f83e937a
olivia <soylee0314@gmail.com>
2020-06-04 19:12:59 +0900

    - 금요일에 네개를... 해보자...

---
+++ app/src/main/java/net/soy/algorithm/level2/brute_force_search/find_prime_number/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level2.brute_force_search.find_prime_number
+
+/**
+ * Class: Solution
+ * Created by leesoyoung on 2020/06/04.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 level2 완전탐색 "소수 찾기" 문제
+ */
+class Solution {
+    fun solution(numbers: String): Int {
+        var answer = 0
+        return answer
+    }
+}
---
+++ app/src/main/java/net/soy/algorithm/level2/stack_queue/truck_across_bridge/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level2.stack_queue.truck_across_bridge
+
+/**
+ * Class: Solution
+ * Created by leesoyoung on 2020/06/04.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 level2 스택/큐 "다리를 지나는 트럭" 문제
+ */
+class Solution {
+    fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        var answer = 0
+        return answer
+    }
+}

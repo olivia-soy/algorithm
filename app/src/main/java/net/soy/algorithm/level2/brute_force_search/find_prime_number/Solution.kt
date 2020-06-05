6a84bd459f09bc2e19eec9559df950b5e672bb4f
olivia <soylee0314@gmail.com>
2020-06-05 16:51:31 +0900

    - 코딩테스트 문제 연습 "소수찾기" -> 문제 통과 적용

--- app/src/main/java/net/soy/algorithm/level2/brute_force_search/find_prime_number/Solution.kt
+++ app/src/main/java/net/soy/algorithm/level2/brute_force_search/find_prime_number/Solution.kt
@@ -1,5 +1,8 @@
 package net.soy.algorithm.level2.brute_force_search.find_prime_number
 
+import java.lang.Math.pow
+import kotlin.math.sqrt
+
 /**
  * Class: Solution
  * Created by leesoyoung on 2020/06/04.
@@ -8,7 +11,51 @@ package net.soy.algorithm.level2.brute_force_search.find_prime_number
  */
 class Solution {
     fun solution(numbers: String): Int {
-        var answer = 0
-        return answer
+
+        //중복 제거를 위해 HashSet 사용
+        val set: HashSet<Int> = HashSet()
+
+        permutation("", numbers, set)
+
+
+
+        val answer: List<Int> = set.filter { isPrime(it) }
+
+        return answer.size
+    }
+
+
+    /**
+     * 소수인지 확인하는 메소드
+     * 소수 : 자신과 1이외의 어떤수로도 나누어 떨어지지 않는 정수
+     */
+    fun isPrime(num: Int): Boolean{
+        if(num == 1 || num == 0) return false
+        for(i in 2 until num){
+            if(num % i == 0){
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * 조합
+     */
+    fun permutation(
+        prefix: String,
+        numbers: String,
+        set: HashSet<Int>
+    ) {
+
+        if (prefix != "") set.add(prefix.toInt())
+
+        for (i in numbers.indices) {
+            permutation(
+                prefix + numbers[i],
+                numbers.substring(0, i) + numbers.substring(i + 1),
+                set
+            )
+        }
     }
 }

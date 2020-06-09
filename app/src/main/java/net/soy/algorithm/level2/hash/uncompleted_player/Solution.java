ca4a6a880cadd9c4a51acf69febfa7cd21b28ef1
olivia <soylee0314@gmail.com>
2020-06-09 16:05:47 +0900

    - 코딩테스트 문제 연습 "다리 위 트럭의 무게" -> 문제 통과 적용 - 코딩테스트 문제 연습 "완주하지 못한 선수" -> 문제 통과 적용

---
+++ app/src/main/java/net/soy/algorithm/level2/hash/uncompleted_player/Solution.java
@@ -0,0 +1,40 @@
+package net.soy.algorithm.level2.hash.uncompleted_player;
+
+import java.util.HashMap;
+
+/**
+ * Class: Solution
+ * Created by leesoyoung on 2020/06/09.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 level1 해시 "완주하지 못한 선수" 문제
+ */
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        //동명 이인 선수를 골라내기 위해 hashMap 으로 저장
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        //참가자의 이름이 있는경우 value 값을 +1해준다
+        for(String part: participant){
+            if(hashMap.containsKey(part)){
+                hashMap.put(part, hashMap.get(part) + 1);
+            } else {
+                hashMap.put(part, 1);
+            }
+        }
+
+        //완주자의 이름이 있는경우 value 값을 -1해준다
+        for(String comp: completion){
+            if(hashMap.containsKey(comp)){
+                hashMap.put(comp, hashMap.get(comp) - 1);
+            }
+        }
+
+        //hashMap value 값 중 0이 아닌경우 완주를 못한것이므로 해당 값 return
+        for(String part: participant){
+            if(hashMap.get(part) != 0)
+                return part;
+        }
+
+        return null;
+    }
+}
--- app/src/main/java/net/soy/algorithm/level2/stack_queue/truck_across_bridge/Solution.kt
+++ app/src/main/java/net/soy/algorithm/level2/stack_queue/truck_across_bridge/Solution.kt
@@ -1,14 +1,62 @@
 package net.soy.algorithm.level2.stack_queue.truck_across_bridge
 
+import java.util.*
+
 /**
  * Class: Solution
- * Created by leesoyoung on 2020/06/04.
+ * Created by leesoyoung on 2020/06/09.
  *
  * Description: 프로그래머스 코딩테스트 연습 level2 스택/큐 "다리를 지나는 트럭" 문제
  */
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
-        var answer = 0
-        return answer
+
+        val outsideTruck = LinkedList<Int>()    // 바깥에서 대기하고 있는 트럭
+        val insideTruck = ArrayList<Truck>()    // 다리 안에서 이동중인 트럭
+
+        //트럭의 무게를 바깥에서 대기중인 트럭 리스트에 다 넣어 준다
+        truck_weights.forEach {
+            outsideTruck.add(it)
+        }
+
+        var totalWeight: Int = outsideTruck.peek()  // 현재 다리 위 무게 -> 대기중인 트럭중 첫번째 트럭의 무게를 더해준다.
+
+        var time = 0    //이동시간
+
+        //대기중인 트럭중 첫 번째 트럭을 다리 위 이동중인 트럭 리스트에 넣어준다, 위치 = 0
+        insideTruck.add(Truck(outsideTruck.poll(), 0))
+
+        while (insideTruck.isNotEmpty()) {   //다리 위 이동중인 트럭 리스트가 있을동안만 진행
+            time++  //이동 시간을 하나씩 더해준다.
+
+            // 다리 위 이동중인 트럭 리스트의 위치 값을 하나씩 증가 시켜준다.
+            insideTruck.forEach {
+                it.index++
+            }
+
+            //  다리 위 이동중인 트럭이 끝까지 갔을 경우
+            //  전체 무게에서 해당 트럭의 무게를 빼주고,
+            //  다리 위 이동중인 트럭 리스트에서 해당 데이터를 지운다
+            if (insideTruck[0].index > bridge_length) {
+                totalWeight -= insideTruck[0].weight
+                insideTruck.removeAt(0)
+            }
+
+            // 대기중인 트럭이 있고
+            // 다리 위 전체 무게 + 대기중인 트럭중 첫번째 트럭의 무게가 다리라 견딜수 있는 무게 보다 적을 경우
+            if (outsideTruck.isNotEmpty() && totalWeight + outsideTruck.peek() <= weight) {
+                val nextTruck =
+                    outsideTruck.poll() //  대기중인 트럭중 첫번째 트럭을 총 무게에 더하고 이동중인 트럭 리스트에 넣어준다
+                totalWeight += nextTruck
+                insideTruck.add(Truck(nextTruck, 1))
+            }
+
+        }
+        return time
     }
+
+    data class Truck(
+        var weight: Int,    //트럭의 무게
+        var index: Int  //다리 위 트럭의 위치
+    )
 }

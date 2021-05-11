8b600569bef092fbcb390a5cca29b98baa54694c
leesoyoung <soylee0314@gmail.com>
2021-05-11 23:16:27 +0900

    test code 추가

--- app/src/test/java/net/soy/algorithm/ExampleUnitTest.kt
+++ app/src/test/java/net/soy/algorithm/ExampleUnitTest.kt
@@ -13,6 +13,12 @@ class ExampleUnitTest {
 
     @Test
     fun solution() {
-
+        val a: IntArray = intArrayOf(-1,0,1)
+        val b: IntArray = intArrayOf(1,0,-1)
+        var answer = 0
+        a.forEachIndexed { aIndex, _ ->
+            answer += a[aIndex] * b[aIndex]
+        }
+        println(answer)
     }
 }

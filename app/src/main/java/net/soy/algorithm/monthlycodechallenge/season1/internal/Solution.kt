cb76794a0b6919f27c712310a1bfaff6ab6272b6
leesoyoung <soylee0314@gmail.com>
2021-05-11 23:14:23 +0900

    코딩테스트 연습 > 월간 코드 챌린지 시즌1 > 내적

---
+++ app/src/main/java/net/soy/algorithm/monthlycodechallenge/season1/internal/Solution.kt
@@ -0,0 +1,17 @@
+package net.soy.algorithm.monthlycodechallenge.season1.internal
+
+/**
+ * Class: Solution
+ * Created by leesoyoung on 2021/05/11.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 월간챌린지 시즌1 내적 문제
+ */
+class Solution {
+    fun solution(a: IntArray, b: IntArray): Int {
+        var answer = 0
+        a.forEachIndexed { aIndex, _ ->
+            answer += a[aIndex] * b[aIndex]
+        }
+        return answer
+    }
+}

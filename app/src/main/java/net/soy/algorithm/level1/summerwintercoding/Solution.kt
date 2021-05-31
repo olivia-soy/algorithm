ffc1a0cb25e9176293f20d8f87829486a17bbeef
leesoyoung <soylee0314@gmail.com>
2021-06-01 08:09:36 +0900

    [코딩테스트 문제 연습/level1] : Summer/Winter Coding(~2018) 소수 만들기 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level1/summerwintercoding/Solution.kt
@@ -0,0 +1,19 @@
+package net.soy.algorithm.level1.summerwintercoding
+
+/**
+ * Class: Solution
+ * Created by leesoyoung on 2021/06/01.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 Summer/Winter Coding(~2018) 소수 만들기
+ *
+ */
+class Solution {
+    fun solution(nums: IntArray): Int {
+        var answer = -1
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        println("Hello Kotlin")
+
+        return answer
+    }
+}

a951f3af7b7b812546107e39ed6362c54f8d7539
leesoyoung <soylee0314@gmail.com>
2021-06-15 12:55:34 +0900

    [코딩테스트 문제 연습/level1] : 2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level1/kakaoblindrecruitment/Solution.kt
@@ -0,0 +1,15 @@
+package net.soy.algorithm.level1.kakaoblindrecruitment
+
+/**
+ * Class: Solution
+ * Created by leesoyoung on 2021/06/15.
+ *
+ * Description: 프로그래머스 코딩테스트 연습 2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천
+ *
+ */
+class Solution {
+    fun solution(new_id: String): String {
+        var answer: String = ""
+        return answer
+    }
+}

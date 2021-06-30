3b77371e2ef638379831c248beb9f93911578128
sylee0314-mytalk <sylee0314@mytalk.co.kr>
2021-06-30 12:51:50 +0900

    [코딩테스트 문제 연습/level1] : 2019 카카오 개발자 겨울 인턴십, 크레인 인형뽑기 게임 문제

---
+++ app/src/main/java/net/soy/algorithm/level1/kakaowinterintern/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level1.kakaowinterintern
+
+/**
+ * 프로그래머스 코딩테스트 연습 2019 카카오 개발자 겨울 인턴십, 크레인 인형뽑기 게임
+ *
+ * @author LEESOYOUNG
+ * @since 2021-06-30
+ */
+class Solution {
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        var answer = 0
+        return answer
+    }
+}

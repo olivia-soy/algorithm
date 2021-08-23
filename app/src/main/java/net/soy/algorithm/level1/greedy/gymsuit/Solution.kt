4bb3cf465b52ec60ee326174c5c332df175ee0f0
sylee0314-mytalk <sylee0314@mytalk.co.kr>
2021-08-24 08:52:41 +0900

    [코딩테스트 문제 연습/level1] : 탐욕법(Greedy) 체육복 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level1/greedy/gymsuit/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level1.greedy.gymsuit
+
+/**
+ * 코딩테스트 연습 > 탐욕법(Greedy) >체육복
+ *
+ * @author LEESOYOUNG
+ * @since 2021-08-24
+ */
+class Solution {
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = 0
+        return answer
+    }
+}

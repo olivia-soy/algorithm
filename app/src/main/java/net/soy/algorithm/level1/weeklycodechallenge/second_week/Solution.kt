47b6e5d7423a7481f1b401c4044f1ddc5ef1be15
leesoyoung <soylee0314@gmail.com>
2021-08-26 08:55:34 +0900

    [코딩테스트 문제 연습/level1] : 위클리 챌린지 > 2주차

---
+++ app/src/main/java/net/soy/algorithm/level1/weeklycodechallenge/second_week/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level1.weeklycodechallenge.second_week
+
+/**
+ * 코딩테스트 연습 > 위클리 챌린지 > 2주차
+ *
+ * @author LEESOYOUNG
+ * @since 2021-08-26
+ */
+class Solution {
+    fun solution(scores: Array<IntArray>): String {
+        var answer: String = ""
+        return answer
+    }
+}

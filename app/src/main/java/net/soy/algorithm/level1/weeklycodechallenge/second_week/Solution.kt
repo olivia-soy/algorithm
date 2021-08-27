fa75d092f346ec445a454ee551846e28da7a1590
leesoyoung <soylee0314@gmail.com>
2021-08-27 16:12:44 +0900

    [코딩테스트 문제 연습/level1] : 위클리 챌린지 > 2주차 진행중

--- app/src/main/java/net/soy/algorithm/level1/weeklycodechallenge/second_week/Solution.kt
+++ app/src/main/java/net/soy/algorithm/level1/weeklycodechallenge/second_week/Solution.kt
@@ -1,14 +1,36 @@
 package net.soy.algorithm.level1.weeklycodechallenge.second_week
 
 /**
- * 코딩테스트 연습 > 위클리 챌린지 > 2주차
- *
- * @author LEESOYOUNG
- * @since 2021-08-26
  */
 class Solution {
     fun solution(scores: Array<IntArray>): String {
-        var answer: String = ""
-        return answer
+        val length = scores.size
+        val subLength = scores.first().size
+        val myScore: Array<Array<Int>> = Array(subLength) { Array(length) { 1 } }
+
+
+
+        for (i in 0 until length){
+            for(j in 0 until subLength){
+                myScore[i][j] = scores[j][i]
+            }
+        }
+
+
+
+        println(myScore)
+
+        return ""
+    }
+
+
+    private fun checkScore(score: Int): String{
+       return when(score){
+            in 0 until 50  -> "F"
+            in 50 until 70  -> "D"
+            in 70 until 80  -> "C"
+            in 80 until 90  -> "B"
+            else -> "A"
+        }
     }
 }
--- app/src/test/java/net/soy/algorithm/SolutionTest.kt
+++ app/src/test/java/net/soy/algorithm/SolutionTest.kt
@@ -305,5 +305,15 @@ class SolutionTest {
         )
     }
 
+    @Test
+    fun secondWeekTest(){
+        val secondWeek = net.soy.algorithm.level1.weeklycodechallenge.second_week.Solution()
+
+        assertEquals(
+            expected = secondWeek.solution(arrayOf(intArrayOf(100,90,98,88,65), intArrayOf(50,45,99,85,77),intArrayOf( 47,88,95,80,67),intArrayOf(61,57,100,80,65), intArrayOf(24,90,94,75,65))),
+            actual = ""
+        )
+    }
+
 }
 

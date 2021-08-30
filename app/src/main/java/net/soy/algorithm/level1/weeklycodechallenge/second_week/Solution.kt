cea83dff586920c4b95cf65f0f37e3bc9f8a8c00
leesoyoung <soylee0314@gmail.com>
2021-08-30 10:07:43 +0900

    [코딩테스트 문제 연습/level1] : 위클리 챌린지 > 2주차 완료

--- app/src/main/java/net/soy/algorithm/level1/weeklycodechallenge/second_week/Solution.kt
+++ app/src/main/java/net/soy/algorithm/level1/weeklycodechallenge/second_week/Solution.kt
@@ -1,6 +1,12 @@
 package net.soy.algorithm.level1.weeklycodechallenge.second_week
 
+import java.util.*
+
 /**
+ * 코딩테스트 연습 > 위클리 챌린지 > 2주차
+ *
+ * @author LEESOYOUNG
+ * @since 2021-08-25
  */
 class Solution {
     fun solution(scores: Array<IntArray>): String {
@@ -9,27 +15,54 @@ class Solution {
         val myScore: Array<Array<Int>> = Array(subLength) { Array(length) { 1 } }
 
 
-
-        for (i in 0 until length){
-            for(j in 0 until subLength){
+        //내가 받은 점수 저장
+        for (i in 0 until length) {
+            for (j in 0 until subLength) {
                 myScore[i][j] = scores[j][i]
             }
         }
 
+        var answer = ""
+        myScore.forEachIndexed { index, ints ->
+            var flag = false
+            var sum = ints.sum()
 
+            //내가 나에게 준 점수
+            val toMy = myScore[index][index]
 
-        println(myScore)
+            /**
+             * Array 정렬
+             * 첫번째 원소 -> 최소값
+             * 마지막 원소 -> 최대값
+             */
+            Arrays.sort(ints)
+            if (toMy >= ints[ints.size - 1] || toMy <= ints[0]) {
+                // 내가 나에제 준 점수와 같은 값이 있는지 확인
+                if (ints.filter { it == toMy }.size == 1) {
+                    flag = true
+                    sum -= toMy
+                }
+            }
 
-        return ""
+            /**
+             * flag
+             * true -> 내가 나에게 준 점수 제거된 상태
+             * false -> sum 에 변화가 없는 상태
+             */
+            answer += checkScore(sum.div(if (flag) ints.size - 1 else ints.size))
+        }
+
+        return answer
     }
 
 
-    private fun checkScore(score: Int): String{
-       return when(score){
-            in 0 until 50  -> "F"
-            in 50 until 70  -> "D"
-            in 70 until 80  -> "C"
-            in 80 until 90  -> "B"
+    private fun checkScore(score: Int): String {
+        println("score : $score")
+        return when (score) {
+            in 0 until 50 -> "F"
+            in 50 until 70 -> "D"
+            in 70 until 80 -> "C"
+            in 80 until 90 -> "B"
             else -> "A"
         }
     }
--- app/src/test/java/net/soy/algorithm/SolutionTest.kt
+++ app/src/test/java/net/soy/algorithm/SolutionTest.kt
@@ -310,8 +310,8 @@ class SolutionTest {
         val secondWeek = net.soy.algorithm.level1.weeklycodechallenge.second_week.Solution()
 
         assertEquals(
-            expected = secondWeek.solution(arrayOf(intArrayOf(100,90,98,88,65), intArrayOf(50,45,99,85,77),intArrayOf( 47,88,95,80,67),intArrayOf(61,57,100,80,65), intArrayOf(24,90,94,75,65))),
-            actual = ""
+            expected = secondWeek.solution(arrayOf(intArrayOf(70,49,90), intArrayOf(68,50,38), intArrayOf(73,31,100))),
+            actual = "CFD"
         )
     }
 

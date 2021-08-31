bda4f55479059abf5424195d6c157b4f905014d7
leesoyoung <soylee0314@gmail.com>
2021-08-31 09:05:52 +0900

    [코딩테스트 문제 연습/level2] : 정렬 > H-Index 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level2/sort/h_index/Solution.kt
@@ -0,0 +1,16 @@
+package net.soy.algorithm.level2.sort.h_index
+
+import java.util.*
+
+/**
+ * 코딩테스트 연습 > 정렬 > H-Index
+ *
+ * @author LEESOYOUNG
+ * @since 2021-08-31
+ */
+class Solution {
+    fun solution(citations: IntArray): Int {
+        var answer = 0
+        return answer
+    }
+}

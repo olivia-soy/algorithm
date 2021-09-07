0818c2181748d2cf3925d39d659c89453b8710f1
leesoyoung <soylee0314@gmail.com>
2021-09-07 10:02:37 +0900

    [코딩테스트 문제 연습/level3] : 해시 > 베스트 앨범 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level3/hash/bestAlbum/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level3.hash.bestAlbum
+
+/**
+ * 코딩테스트 연습 > 해시 > 베스트 앨범
+ *
+ * @author LEESOYOUNG
+ * @since 2021-09-07
+ */
+class Solution {
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+        var answer = intArrayOf()
+        return answer
+    }
+}

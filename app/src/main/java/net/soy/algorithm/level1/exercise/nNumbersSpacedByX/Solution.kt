d7bb520d692c6d0c182a38b19e47e2d2de1183b6
leesoyoung <soylee0314@gmail.com>
2021-09-14 17:56:26 +0900

    [코딩테스트 문제 연습/level1] : 연습 문제 > x만큼 간격이 있는 n개의 숫자 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level1/exercise/nNumbersSpacedByX/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level1.exercise.nNumbersSpacedByX
+
+/**
+ * Algorithm
+ * 코딩테스트 연습 > 연습 문제 > x만큼 간격이 있는 n개의 숫자
+ * @author LEESOYOUNG
+ * @since 2021-09-14
+ */
+class Solution {
+    fun solution(x: Int, n: Int): LongArray {
+        var answer = longArrayOf()
+        return answer
+    }
+}

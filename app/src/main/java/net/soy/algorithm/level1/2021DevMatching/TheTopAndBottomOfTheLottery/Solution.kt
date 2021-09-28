63c910a9b24ed4aa9a785f35d38880b89a84c509
leesoyoung <soylee0314@gmail.com>
2021-09-28 10:37:24 +0900

    [코딩테스트 문제 연습/level1] : 2021 Dev-Matching: 웹 백엔드 개발 > 로또의 최고 순위와 최저 순위 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level1/2021DevMatching/TheTopAndBottomOfTheLottery/Solution.kt
@@ -0,0 +1,14 @@
+package net.soy.algorithm.level1.`2021DevMatching`.TheTopAndBottomOfTheLottery
+
+/**
+ * Algorithm
+ * 코딩테스트 연습 > 2021 Dev-Matching: 웹 백엔드 개발 > 로또의 최고 순위와 최저 순위
+ * @author LEESOYOUNG
+ * @since 2021-09-28
+ */
+class Solution {
+    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        return answer
+    }
+}

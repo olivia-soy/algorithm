36faa1635b5687a3ae5e2e394adb7ea7af26664e
leesoyoung <soylee0314@gmail.com>
2021-10-26 08:48:30 +0900

    [코딩테스트 문제 연습/level2] : 힙 > 더 맵게 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level2/heapSort/moreSpicy/Solution.java
@@ -0,0 +1,15 @@
+package net.soy.algorithm.level2.heapSort.moreSpicy;
+
+/**
+ * Algorithm
+ * 코팅테스트 연습 > 힙(Heap) > 더 맵게
+ *
+ * @author LEESOYOUNG
+ * @since 2021-09-30
+ */
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+}

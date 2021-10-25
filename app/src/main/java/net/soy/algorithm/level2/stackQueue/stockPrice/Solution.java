7e384711431315d52ceec8a45f6464f3ba90ab0b
leesoyoung <soylee0314@gmail.com>
2021-10-25 09:10:50 +0900

    [코딩테스트 문제 연습/level2] : 스택/큐 > 주식가격 풀이 완료

--- app/src/main/java/net/soy/algorithm/level2/stackQueue/stockPrice/Solution.java
+++ app/src/main/java/net/soy/algorithm/level2/stackQueue/stockPrice/Solution.java
@@ -3,12 +3,19 @@ package net.soy.algorithm.level2.stackQueue.stockPrice;
 /**
  * Algorithm
  * 코팅테스트 연습 > 스택/큐 > 주식 가격
+ *
  * @author LEESOYOUNG
  * @since 2021-09-30
  */
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int[] answer = new int[prices.length];
+        for(int i = 0; i< prices.length ; i++) {
+            for (int j =i+1; j < prices.length; j++) {
+                answer[i]++;
+                if(prices[i]>prices[j])break;
+            }
+        }
         return answer;
     }
 }

419bde3d9002c3981eeac4a5190a6a1d3b21edd7
leesoyoung <soylee0314@gmail.com>
2021-12-01 08:57:57 +0900

    [코딩테스트 문제 연습/level2] : 힙 > 더 맵게 문제 풀이 완료

--- app/src/main/java/net/soy/algorithm/level2/heapSort/moreSpicy/Solution.java
+++ app/src/main/java/net/soy/algorithm/level2/heapSort/moreSpicy/Solution.java
@@ -1,8 +1,12 @@
 package net.soy.algorithm.level2.heapSort.moreSpicy;
 
+import java.util.PriorityQueue;
+
 /**
  * Algorithm
  * 코팅테스트 연습 > 힙(Heap) > 더 맵게
+ * <p>
+ * 우선순위 큐 문제 : Priority Queue
  *
  * @author LEESOYOUNG
  * @since 2021-09-30
@@ -10,6 +14,27 @@ package net.soy.algorithm.level2.heapSort.moreSpicy;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        PriorityQueue<Integer> priorityQueueLowest = new PriorityQueue<>();
+
+        for (int j : scoville) {
+            priorityQueueLowest.add(j);
+        }
+
+        while (priorityQueueLowest.peek() < K) {
+            try {
+                int firstLow = priorityQueueLowest.poll();
+                int secondLow = priorityQueueLowest.poll();
+                priorityQueueLowest.add(mixScoville(firstLow, secondLow));
+                answer += 1;
+            } catch (NullPointerException e) {
+                return -1;
+            }
+        }
+
         return answer;
+    }
+
+    private int mixScoville(int firstLow, int secondLow) {
+        return firstLow + (secondLow * 2);
     }
 }

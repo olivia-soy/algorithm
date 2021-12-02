725e468345ed837407f7de24f6a4339cc8f8c03c
leesoyoung <soylee0314@gmail.com>
2021-12-02 09:02:38 +0900

    [코딩테스트 문제 연습/level3] : 힙 > 디스크 컨트롤러 문제 추가

---
+++ app/src/main/java/net/soy/algorithm/level3/heap/diskControl/Solution.java
@@ -0,0 +1,15 @@
+package net.soy.algorithm.level3.heap.diskControl;
+
+/**
+ * Algorithm
+ * 코팅테스트 연습 > 힙(Heap) > 디스크 컨트롤러
+ *
+ * @author LEESOYOUNG
+ * @since 2021-12-02
+ */
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
+}

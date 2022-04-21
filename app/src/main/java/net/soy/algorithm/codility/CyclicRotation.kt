e6caca781232e2e86c242132e74a4e65d86142ef
leesoyoung <soylee0314@gmail.com>
2022-04-21 23:47:36 +0900

    [Codility] : Edit Comment

--- app/src/main/java/net/soy/algorithm/codility/CyclicRotation.kt
+++ app/src/main/java/net/soy/algorithm/codility/CyclicRotation.kt
@@ -8,42 +8,7 @@ package net.soy.algorithm.codility
  */
 class CyclicRotation {
     /**
-     * An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
-
-    The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
-
-    Write a function:
-
-    fun solution(A: IntArray, K: Int): IntArray
-
-    that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
-
-    For example, given
-
-    A = [3, 8, 9, 7, 6]
-    K = 3
-    the function should return [9, 7, 6, 3, 8]. Three rotations were made:
-
-    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
-    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
-    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
-    For another example, given
-
-    A = [0, 0, 0]
-    K = 1
-    the function should return [0, 0, 0]
-
-    Given
-
-    A = [1, 2, 3, 4]
-    K = 4
-    the function should return [1, 2, 3, 4]
-
-    Assume that:
-
-    N and K are integers within the range [0..100];
-    each element of array A is an integer within the range [−1,000..1,000].
-    In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+     * N 개의 정수와 정수 K로 구성된 배열 A가 주어지면 배열 A를 회전 K 번 반환합니다.
      */
     fun solution(A: IntArray, K: Int): IntArray {
         val answerArray = IntArray(A.size)
